ba260ff5b53dc86833aeab37003b8a1ec9a50a9f
Kevin Lee <kevinmlee.x@gmail.com>
2014-01-15 13:06:41 -0800

    sizing and spacing changes for 1080p

--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,5 @@ $( document ).ready(function() {
 
 function parallaxScroll() {
 	var dist = $(window).scrollTop();
-	$('#logo-bg').css('background-position', '50% ' + (0-(dist*1.5))+'px');
-	$('.fade').css('top', (0-(dist*0.5))+'px');
+	$('#logo-bg').css('background-position', '50% ' + (0-(dist*1.25))+'px');
 }
