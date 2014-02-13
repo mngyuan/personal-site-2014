d60305d0e2e1cf8558d1966bcadc42ff09246332
Kevin Lee <kevinmlee.x@gmail.com>
2014-02-12 23:35:41 -0800

    fixed sidebar size

--- js/nav.js
+++ js/nav.js
@@ -7,5 +7,6 @@ $( document ).ready( function() {
 
 function resizeSideAndContent() {
     var vpheight = $(window).height();
-    $('#blog_sidebar, #blogcontent, #sidebar_vcenter').css('height', vpheight);
+    $('#blog_sidebar, #blogcontent').css('height', vpheight);
+    $('#sidebar_vcenter').css('height', vpheight - 2*parseInt($('#sidebar_vcenter').css('padding-top'), 10));
 }
